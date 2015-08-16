ecad48ced4e9be31142c07c2580edbc9c66430c4
Andrew D <retroburst@gmail.com>
2015-08-16 13:11:01 +0900

    Initial development in progress

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+	public float initialVelocity = 600f;
+	private bool inPlay = false;
+	private Rigidbody rb = null;
+	
+	// Use this for initialization
+	private void Awake () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1") && !inPlay){
+			transform.parent = null;
+			inPlay = true;
+			rb.isKinematic = false;
+			rb.AddForce(new Vector3(Random.Range(-initialVelocity, initialVelocity), initialVelocity, 0f));
+		}
+	}
+	
+	void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.tag == "Ena")
+		{
+			GameObject.Destroy(collision.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleController : MonoBehaviour {
+	public float paddleSpeed = 1;
+	public float minHorizontalClamp = -8f;
+	public float maxHorizontalClamp = 8f;
+	private Vector3 playerPosition = Vector3.zero;
+	
+	// Update is called once per frame
+	private void Update () {
+		playerPosition = transform.position;
+		float xPosition = transform.position.x + Input.GetAxis("Horizontal") * paddleSpeed;
+		playerPosition = new Vector3(Mathf.Clamp(xPosition, minHorizontalClamp, maxHorizontalClamp), playerPosition.y, playerPosition.z);
+		transform.position = playerPosition;
+	}
+}
